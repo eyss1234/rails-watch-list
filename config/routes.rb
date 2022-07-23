c90a64918071ae56b8087d28cf3cde1b6617d38c
Emily Santos <eyssantos@gmail.com>
2022-07-23 15:25:22 +0100

    Setting up the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :lists, only: %i[index show new create] do
-    resources :movies, only: %i[index show]
-    resources :bookmark, only: %i[index show]
+    resources :bookmark, only: %i[index show new create]
   end
 end
